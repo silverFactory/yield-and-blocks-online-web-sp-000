1e51f78342213b52083b17eee9ace2781aad2663
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 20:40:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(array)
+  if block_given?
+    
+  end
   i = 0
   while i< array.length
     yield array[i]
     i= i + 1
+  else
+    puts "Hey! No block was given!"
   end
-  array.length != nil ? array : "Hey! No block was given!"
 end
 
 # call your method here!
